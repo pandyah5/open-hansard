b1188096cacfc81e1ce5661fb8f2db1d72e898b2
pandyah5 <pandyahetav1@gmail.com>
2024-05-24 19:03:49 -0700

    Update content - 2024-05-23

--- run.ps1
+++ run.ps1
@@ -1,17 +1,17 @@
 # Run main.py
 python auto-hansard/main.py > debug.txt
 
-# # Use today's date for creating a new content file
-$today = Get-Date -Format "yyyy-MM-dd"
-hugo new "summary\Debate-$today.md"
+# Use yesterday's date for creating a new content file
+$yesterday = (Get-Date).AddDays(-1).ToString("yyyy-MM-dd")
+hugo new "summary\Debate-$yesterday.md"
 
 # Add contents of the new file to Debate-$today.md
-Get-Content "auto-hansard\summary\Debate-$today.md" | Add-Content "content\summary\Debate-$today.md"
+Get-Content "auto-hansard\summary\Debate-$yesterday.md" | Add-Content "content\summary\Debate-$yesterday.md"
 
 # Set the draft to false
-(Get-Content "content\summary\Debate-$today.md") -replace "draft = true", "draft = false" | Set-Content "content\summary\Debate-$today.md"
+(Get-Content "content\summary\Debate-$yesterday.md") -replace "draft = true", "draft = false" | Set-Content "content\summary\Debate-$yesterday.md"
 
 # Run a git command to add, commit and push the changes
 git add .
-git commit -m "Update content - $today"
+git commit -m "Update content - $yesterday"
 git push
--- run.sh
+++ run.sh
@@ -3,18 +3,17 @@
 # Run main.py
 python auto-hansard/main.py > debug.txt
 
-# Use today's date for creating a new content file
-today=$(date '+%Y-%m-%d')
-hugo new summary/Debate-$today.md
+# Use yesterday's date for creating a new content file
+yesterday=$(date -d "yesterday 13:00" '+%Y-%m-%d')
+hugo new summary/Debate-$yesterday.md
 
 # Add contents of the new file to Debate-$today.md
-cat auto-hansard/summary/Debate-$today.md > open-hansard-website/content/summary/Debate-$today.md
+cat auto-hansard/summary/Debate-$yesterday.md > open-hansard-website/content/summary/Debate-$yesterday.md
 
 # Set the draft to false
-sed -i 's/draft: true/draft: false/g' content/summary/Debate-$today.md
+sed -i 's/draft: true/draft: false/g' content/summary/Debate-$yesterday.md
 
 # Run a git command to add, commit and push the changes
-git add auto-hansard/summary/*
-git add open-hansard-website/content/summary/*
-git commit -m "Update content"
+git add .
+git commit -m "Update content - $yesterday"
 git push
